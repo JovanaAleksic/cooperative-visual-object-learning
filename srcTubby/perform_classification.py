140b5ac47fc50b68cc011a77367093a9b39a6232
Jovana Aleksic <53390106+JovanaAleksic@users.noreply.github.com>
2022-09-16 13:35:17 +0300

    Delete srcTubby directory

--- srcTubby/perform_classification.py
+++
@@ -1,8 +0,0 @@
-import caffe
-
-def clas(transformer,net, a):
-    #this function performs clasification
-    transformed_image = transformer.preprocess('data', a)
-    net.blobs['data'].data[...] = transformed_image # copy the image data into the memory allocated for the net
-
-    return net.forward()  # perform classification
