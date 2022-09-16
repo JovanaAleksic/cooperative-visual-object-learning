00960c063fa1d73bc4b8ba1d15ee336c3d984aeb
Jovana Aleksic <53390106+JovanaAleksic@users.noreply.github.com>
2022-09-16 13:34:48 +0300

    Delete src directory

--- src/get_treshold.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-objId_list, objId_test_list, neuron_id, n_neighbors, f1, k, limit_test_views, repeat, treshold, local_h, perse, treshold3 = pickle.load(open("Strategies/Test3/Test162.pickle", "rb"))
-
-print treshold3
